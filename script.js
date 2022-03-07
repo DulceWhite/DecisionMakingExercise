8e564fd6adcbe11c6d0fc028b47856333606cf7c
Dulce <dulce.a.suarez@gmail.com>
2022-03-07 09:19:16 -0600

    Some important notes added and a correciton on ternary

--- script.js
+++ script.js
@@ -4,28 +4,41 @@
 let randomNum = Math.floor(Math.random()*5)+1;
 
 console.log(randomNum);
+//OR
+
+/*
+let randomNum = Math.random();
+randomNum *= 5;
+randomNum = Math.floor(randomNum);
+randomNum ++;
+*/
 
 //1b
 if (randomNum >= 4){
     console.log(`Greater than or equal to 4`);
-} else if (randomNum == 3 || randomNum == 2){
+} else if (randomNum === 3 || randomNum === 2){//best practice use === strict equality
     console.log(`Equal to 2 or 3`);
 } else {
     console.log(`Equal to 1`);
 }
 
 //1c
-if (randomNum != 3){
+if (randomNum !== 3){//use strict 
     console.log(`Not equal to 3`);
 }
 
 //1d
-if (randomNum != 3 && randomNum != 5){
+if (randomNum !== 3 && randomNum !== 5){
+    console.log(`Not equal to 3 AND not equal to 5`);
+}
+//OR
+
+if (randomNum !==3 && 5){
     console.log(`Not equal to 3 AND not equal to 5`);
 }
 
 //1e
-if (randomNum == 2 || randomNum == 4){
+if (randomNum === 2 || randomNum === 4){
     console.log(`Equal to 2 OR equal to 4`);
 }
 
@@ -35,8 +48,9 @@ if (randomNum == 2 || randomNum == 4){
 
 //2
 
-// randomNum = (randomNum >= 4)? "Greater than or equal to 4": "Less than 4";
-// console.log(randomNum);
+randomNum >= 4? "Greater than or equal to 4" : "Less than 4";
+console.log(randomNum);
+
 
 if(randomNum >=4){
     console.log(`Greater than or equal to 4`);
@@ -44,12 +58,17 @@ if(randomNum >=4){
     console.log(`Less than 4`);
 }
 
+//or
+randomNum >= 4 ? console.log(`Greater than or equal to 4`): console.log(`Less than 4`);
+
 //3
+//Switch statement takes a parameter/argument and returns something based on the CASE (or match) of the parameter/argument goes with 
+//In this example if randomNum is 1 it will display "one" in the terminal , if randomNum is 2 it will display "two" etc.
 
 switch (randomNum){
     case 1:
         console.log(`One`);
-        break;
+        break;//breaks are needed between each case so that the code for the case that follows does NOT also run
     case 2:
         console.log(`Two`);
         break;
@@ -61,7 +80,6 @@ switch (randomNum){
         break;
     case 5:
         console.log(`Five`);
-        break;
+        //There is no need of a break at the end of a switch statement since there are no other cases.
  
-
 }
